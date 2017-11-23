0afcf568e981965e1a1edc0ebf7e41558974f029
Arjun Nayini <aanayini@gmail.com>
2017-11-22 22:26:44 -0800

    Initial work on scanner

---
+++ Sources/swift-lox/Lox.swift
@@ -0,0 +1,64 @@
+//
+//  Lox.swift
+//  swift-loxPackageDescription
+//
+//  Created by Arjun Nayini on 11/22/17.
+//
+
+import Foundation
+
+public class Lox {
+    static var hadError = false
+    //
+    //    public static func run() {
+    //        let arguments = CommandLine.arguments
+    //        // CommandLine.arguments always has a first
+    //        // argument of the path to the executable
+    //        switch arguments.count {
+    //        case 1:
+    //            // No user supplied argument
+    //            // just run the REPL
+    //            runPrompt()
+    //        case 2:
+    //            runFile(filename: arguments[0])
+    //        default:
+    //            print("Usage: swiftlox [script])")
+    //        }
+    //    }
+    
+    static func error(line: Int, message: String) {
+        self._report(line: line, location: "", message: message)
+    }
+    
+    static private func _report(line: Int, location: String, message: String) {
+        print("[line \(line)] Error \(location) : \(message)")
+    }
+    
+    //    static void error(int line, String message) {
+    //    report(line, "", message);
+    //    }
+    //
+    //    static private void report(int line, String where, String message) {
+    //    System.err.println(
+    //    "[line " + line + "] Error" + where + ": " + message);
+    //    hadError = true;
+    //    }
+    
+    static func runPrompt() {
+        print("runPrompt")
+    }
+    
+    static func runFile(filename: String) {
+        print("runFile \(filename)")
+    }
+    
+    public static func run(source: String) {
+        let scanner = Scanner(source: source)
+        let tokens = scanner.scanTokens()
+        
+        for token in tokens {
+            print(token)
+        }
+    }
+}
+
--- Sources/swift-lox/main.swift
+++ Sources/swift-lox/main.swift
@@ -1,1 +1,5 @@
-print("Hello, world!")
+import Foundation
+
+Lox.run(source: "var x = 4")
+
+
